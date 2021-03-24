d8abf60e21afdaf22aa1042b9015bd3187acd896
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-24 18:53:35 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,30 @@
+const dbd = require("dbd.js")
+
+const bot = new dbd.Bot({
+  mobile: false, // you can change it to "true" if you want a mobile status
+  token: "TOKEN", // change TOKEN with your bot token
+  prefix: ['$getServerVar[prefix]', '<@!$clientID>', '<@$clientID>'] // you may change the bot's main prefix in vars.js
+})
+
+bot.onMessage({
+  guildOnly: false // commands will work in dms. set "true" for commands to work in guilds only
+})
+
+bot.status({
+    text: `with $serverCount servers`, // put any text
+    type: "PLAYING", // LISTENING, PLAYING, WATCHING, COMPETING
+    status: "online", // online, dnd, idle, invisible
+    time: 75 // amount of times where you can change the bot status (if have multiple statuses)
+    })
+
+    // FOR STREAMING STATUS USE THIS
+    // bot.status({
+    // text: "text", 
+    // type: "STREAMING", 
+    // url: "Enter URL"
+    //   })
+    // remove `//` in each side
+
+    bot.variables(require('./commands/vars.js'))
+
+    bot.loadCommands('./commands')
