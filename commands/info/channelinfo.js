601c6c80777fc0cabaab19ad75fb66d058a8a7e8
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-24 18:51:59 +0800

    Create channelinfo.js

---
+++ commands/info/channelinfo.js
@@ -0,0 +1,20 @@
+module.exports = ({
+    name: "channelinfo",
+    aliases: ['channelinformation', 'chnlinformation', 'chnlinfo'],
+    description: "Know information about a specific server channel",
+    usage: "channelinfo <channel>",
+    category: "info",
+    code: `$color[RANDOM]
+    $author[$userTag;$authorAvatar]
+    $title[Channel Information]
+    $addField[Viewable; $replaceText[$replaceText[$checkCondition[$channel[$findServerChannel[$message];isviewable]==true];true;Yes];false;No];yes]
+    $addField[ID; $findServerChannel[$message];yes]
+    $addField[Position; $channel[$findServerChannel[$message];rawposition];yes]
+    $addField[Topic; $channelTopic[$findServerChannel[$message]];yes]
+    $addField[Type; $toLocaleUpperCase[$channelType[$findServerChannel[$message]]];yes]
+    $addField[Creation Date; $channel[$findServerChannel[$message];created];yes]$addTimestamp
+    $onlyIf[$findServerChannel[$message;no]!=;{description: :x: That server channel doesn't exist!}{color:RANDOM}]
+    $suppressErrors[{description:An error occurred! Please contact the Bot Developer if this keeps happening!}{color:RANDOM}]
+    $cooldown[5s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}{delete:$getCooldownTime[globalUser;channelinfo;$authorID;yes]ms}]
+    $onlyIf[$checkContains[$channelType;text;news]==true;]`
+})
