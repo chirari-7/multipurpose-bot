e42a3e0b4ddffaf986a75935a0c2c402d358e5d7
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-24 18:49:25 +0800

    Create vars.js

---
+++ commands/vars.js
@@ -0,0 +1,10 @@
+const vars = {
+    prefix: ";", // you can change it with desired prefix
+      xp: "0",
+      level: "0",
+      req: "50",
+      expcd: "45",
+      leveling: "enable",
+      money: "0"
+    }
+    module.exports = vars
