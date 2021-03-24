d9c72b4fa9f10f6c641a7a91aba1409a3edad872
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-24 18:50:11 +0800

    Create help.js

---
+++ commands/utility/help.js
@@ -0,0 +1,43 @@
+module.exports = ({
+    name: "help",
+    aliases: ['h', 'commands', 'cmds'],
+    description: "Show the list of commands",
+    usage: "",
+    category: "utility",
+    code:
+    `
+    $thumbnail[$userAvatar[$clientID]]
+    $description[Do \`$getServerVar[prefix]commandinfo <command name>\` to show a command's information
+Utility:
+\`\`\`
+help
+invite
+ping
+setprefix
+\`\`\`
+Leveling:
+\`\`\`
+leveling enable/disable [enabled by default\\]
+levels
+exp
+expcooldown
+\`\`\`
+Economy:
+\`\`\`
+daily
+work
+beg
+balance
+leaderboard
+award [Bot Developer only\\]
+\`\`\`
+Information:
+\`\`\`
+commandinfo
+userinfo
+channelinfo
+roleinfo
+serverinfo
+\`\`\`]
+$color[RANDOM]`
+    })
