858a4cd174c34a0a38e0c794d3e1cd684825650b
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-25 18:31:40 +0800

    Update invite.js

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -2,7 +2,7 @@ module.exports = ({
     name: "invite",
     aliases: ['add'],
     description: "Invite me to your server!",
-    usage: "none",
+    usage: "",
     category: "utility",
     code: `$description[Click [here\\](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=1544023161) to invite me!]
     $color[RANDOM]`
