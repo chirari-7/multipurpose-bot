676fcd86407b5f29bff8d49669d71ae44f23988d
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-29 17:08:20 +0800

    Update leveling.js

--- commands/leveling/leveling.js
+++ commands/leveling/leveling.js
@@ -5,10 +5,11 @@ module.exports = ({
     usage: "leveling <enable/disable>",
     category: "leveling",
     code: `$color[RANDOM]
-    $description[✅ Done! Leveling is now "$replaceText[$replaceText[$checkCondition[$message==enable];true;enabled];false;disabled]".]
-    $setServerVar[leveling;$toLowerCase[$message]]
-    $onlyIf[$message[1]!=; {description: Please specify whether to enable or disable leveling in this server!}{color:RANDOM}]
-    $onlyIf[$getServerVar[leveling]!=$message;{description: Leveling is already $messaged!}{color:RANDOM}]
+    $description[✅ Done! Leveling is now "$getServerVar[leveling]".]
+    $setServerVar[leveling;$toLowerCase[$message[1]]]
+    $onlyIf[$getServerVar[leveling]!=$message[1];{description: Leveling is already $message[1]!}{color:RANDOM}]
+    $onlyIf[$checkContains[$message[1];enable;disable;enabled;disabled]==true;{description: Choose only between "enable" or "disable"!}{color:RANDOM}]
+    $onlyIf[$message[1]!=; {description: Please specify whether to "enable" or "disable" leveling in this server!}{color:RANDOM}]
     $onlyPerms[manageserver;{description:❌ You require these permissions - **Manage Server** }{color:RANDOM}]
     $onlyIf[$checkContains[$channelType;text;news]==true;]`
 })
