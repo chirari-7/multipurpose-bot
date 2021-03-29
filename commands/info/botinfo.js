2c8dec78ea3047c2e6e7078484df246b011c7089
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-29 17:09:22 +0800

    added 'botinfo' cmd 💕

---
+++ commands/info/botinfo.js
@@ -0,0 +1,22 @@
+module.exports = ({
+    name: "botinfo", 
+    aliases: ['botstats'],
+    description: "Check the bot's information/stats",
+    usage: "",
+    category: "info",
+    code: `$title[Botinfo ($username[$clientID])]
+    $color[RANDOM]
+    $thumbnail[$userAvatar[$clientID]]
+    $addField[CPU Usage; $cpu%]
+    $addField[Ping; $ping]
+    $addField[Uptime;$uptime]
+    $addField[Commands; $commandsCount]
+    $addField[Users;$allMembersCount]
+    $addField[Channels;$allChannelsCount]
+    $addField[Servers;$serverCount]
+    $addField[Version;1.0.0]
+    $addField[Created;$creationDate[$clientID]]
+    $addField[Developer;$userTag[$botOwnerID]]
+    $addTimestamp
+    $cooldown[5s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}{delete:$getCooldownTime[globalUser;botinfo;$authorID;yes]ms}]`
+    })
