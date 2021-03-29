485cd2297270da7cb9f9998140f934d38fe06aa9
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-29 17:04:47 +0800

    Update xpcooldown.js

--- commands/leveling/xpcooldown.js
+++ commands/leveling/xpcooldown.js
@@ -10,6 +10,6 @@ module.exports = ({
     $onlyIf[$message!=;{description:You have to specify a message to change the server EXP cooldown!(In seconds)
         Example - $getServerVar[prefix]expcooldown 60 (1 minute)}{color:RANDOM}]
    $onlyIf[$checkContains[$channelType;text;news]==true;]
-   $onlyIf[$getServerVar[leveling]==enable;{description: Leveling is not enabled in this server! To enable it, do \`$getServerVar[prefix]leveling enable\`}{color:RANDOM}]
+   $onlyIf[$checkContains[$getServerVar[leveling];enable;enabled]==true;{description: Leveling is not enabled in this server! To enable it, do \`$getServerVar[prefix]leveling enable\`}{color:RANDOM}]
    $onlyPerms[manageserver;{description:❌ You require these permissions - **Manage Server** }{color:RANDOM}]`
   })
