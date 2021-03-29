99a190c1f65f0f08c68f4b04f1b7fadad9b82d0a
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-29 17:07:31 +0800

    Update levels.js

--- commands/leveling/levels.js
+++ commands/leveling/levels.js
@@ -8,6 +8,6 @@ module.exports = ({
     $description[$userLeaderboard[level;asc;{top}. {username} - Level {value}]]
     $color[RANDOM]
     $cooldown[5s;{description:A bit too fast there. Wait for **time%**!}{color:RANDOM}{delete:$getCooldownTime[globalUser;levels;$authorID;yes]ms}]
-    $onlyIf[$getServerVar[leveling]==enable;{description: Leveling is not enabled in this server! To enable it, do \`$getServerVar[prefix]leveling enable\`}{color:RANDOM}]
+    $onlyIf[$checkContains[$getServerVar[leveling];enable;enabled]==true;{description: Leveling is not enabled in this server! To enable it, do \`$getServerVar[prefix]leveling enable\`}{color:RANDOM}]
     $onlyIf[$checkContains[$channelType;text;news]==true;]`
 })
