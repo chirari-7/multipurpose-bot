b65071f07ed4886991bd4c428371fc4a967d188d
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-29 10:25:18 +0800

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -5,7 +5,7 @@ module.exports = ({
     usage: "play <song/url>",
     category: "music",
     code: `$color[RANDOM]
-    $title[Song added to the Queue!;$songInfo[url]]
+    $title[Song added to the queue!;$songInfo[url]]
     $description[✅ Added **$playSong[$message;1m;yes;yes;{color:RANDOM}{description:An error occurred! Please contact the Bot Developer if this keeps happening!}]**]
     $footer[Requested by $userTag]
     $addTimestamp
@@ -13,5 +13,6 @@ module.exports = ({
     $onlyIf[$message!=;{description: You have to specify a song name/url to play!}{color:RANDOM}]
     $onlyIf[$checkContains[$channelType;text;news]==true;]
     $suppressErrors[{color:RANDOM}{description:An error occurred! Please contact the Bot Developer if this keeps happening!}]
-    $onlyBotPerms[speak;connect;{description:❌ I require these permissions - **Speak** and **Connect**}{color:RANDOM}]`   
+    $onlyBotPerms[speak;connect;{description:❌ I require these permissions - **Speak** and **Connect**}{color:RANDOM}]
+    $botTyping[3s]`   
 })
