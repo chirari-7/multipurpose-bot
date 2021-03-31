f28fb0760ceb8898f191cd30db246f1d0d6bb75a
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-03-31 19:17:28 +0800

    Update roleinfo.js

--- commands/info/roleinfo.js
+++ commands/info/roleinfo.js
@@ -14,7 +14,7 @@ module.exports = ({
     $addField[Position; $role[$findRole[$message];position];yes]
     $addField[Creation Date; $role[$findRole[$message];created];yes]
     $addTimestamp
-    $onlyIf[$findRole[$message[1]]!=;{description: :x: That role doesn't exist!}{color:RANDOM}]
+    $onlyIf[$findRole[$message]!=;{description: :x: That role doesn't exist!}{color:RANDOM}]
     $onlyIf[$message!=;{description:You have to specify a role to see that role's information!}{color:RANDOM}]
     $suppressErrors[{description:An error occurred! Please contact the Bot Developer if this keeps happening!}{color:RANDOM}]
     $cooldown[5s;{description:A bit too fast there. Wait for **%time%**!}{color:RANDOM}{delete:$getCooldownTime[globalUser;roleinfo;$authorID;yes]ms}]
