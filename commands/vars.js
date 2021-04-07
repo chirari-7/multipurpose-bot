ccacc8b37aaff0bd51c056a1cb08e5a58ba2df35
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-04-07 10:22:16 +0800

    Update vars.js

--- commands/vars.js
+++ commands/vars.js
@@ -1,10 +1,9 @@
-const vars = {
-    prefix: ";", // you can change it with desired prefix
-      xp: "0",
-      level: "0",
-      req: "50",
-      expcd: "45",
-      leveling: "enable",
-      money: "0"
-    }
-    module.exports = vars
+module.exports = ({
+  prefix: ";", // you can change it with desired prefix
+    xp: "0",
+    level: "0",
+    req: "50",
+    expcd: "45",
+    leveling: "enabled",
+    money: "0"
+  })
