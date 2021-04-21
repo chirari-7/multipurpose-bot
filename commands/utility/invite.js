02d696bfc57931500d4389cac0b6d62d49c11431
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-04-21 14:40:13 +0800

    Update invite.js

--- commands/utility/invite.js
+++ commands/utility/invite.js
@@ -1,3 +1,5 @@
+// if you're using `dbd.js 2.2.6`, use the code below
+
 module.exports = ({
     name: "invite",
     aliases: ['add'],
@@ -7,3 +9,16 @@ module.exports = ({
     code: `$description[Click [here\\](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=1544023161) to invite me!]
     $color[RANDOM]`
 })
+
+// but if you're using the latest version of dbd.js (dbd.js@latest), use the code below
+// remember to remove the `//` every line below
+
+// module.exports = ({
+//    name: "invite",
+//    aliases: ['add'],
+//    description: "Invite me to your server!",
+//    usage: "",
+//    category: "utility",
+//    code: `$description[Click [here](https://discord.com/oauth2/authorize?client_id=$clientID&scope=bot&permissions=1544023161) to invite me!]
+//    $color[RANDOM]`
+// })
