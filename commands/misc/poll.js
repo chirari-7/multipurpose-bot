73592d84d2e1e3a38053ad76d1700a0bc83f6022
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-04-23 12:02:03 +0800

    Update poll.js

--- commands/misc/poll.js
+++ commands/misc/poll.js
@@ -55,13 +55,12 @@ $replaceText[$replaceText[$checkCondition[$splitText[11]==];false;🔟 - $splitT
 // $replaceText[$replaceText[$checkCondition[$splitText[11]==];false;🔟 - $splitText[11]];true;]]
 //    $addField[Question; $splitText[1]]
 //    $addTimestamp
-//    $addReactions[1️⃣;2️⃣$replaceText[$replaceText[$checkCondition[$splitText[4]==];false;#SEMI#3️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[5]==];false;#SEMI#4️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[6]==];false;#SEMI#5️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[7]==];false;#SEMI#6️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[8]==];false;#SEMI#7️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[9]==];false;#SEMI#8️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[10]==];false;#SEMI#9️⃣];true;]$replaceText[$replaceText[$checkCondition[$splitText[11]==];false;#SEMI#🔟];true;]]
+//    $addReactions[1️⃣;2️⃣;$replaceText[$replaceText[$checkCondition[$splitText[4]==];false;3️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[5]==];false;4️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[6]==];false;5️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[7]==];false;6️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[8]==];false;7️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[9]==];false;8️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[10]==];false;9️⃣];true;];$replaceText[$replaceText[$checkCondition[$splitText[11]==];false;🔟];true;]]
 //    $deletecommand
 //    $textSplit[$message;/]
 //    $onlyIf[$splitText[12]!=false;{description:You can only have up to 10 options!}{color:RANDOM}]
 //    $onlyIf[$message!=;{description: You have to specify a message to make a poll!}{color:RANDOM}]
 //    $onlyBotPerms[addreactions;{description:❌ I require these permissions - **Add Reactions**}{color:RANDOM}]
 //    $onlyPerms[managemessages;{description:❌ You require these permissions - **Manage Messages** }{color:RANDOM}]
-//    $onlyIf[$checkContains[$channelType;text;news]==true;]
-//    `
+//    $onlyIf[$checkContains[$channelType;text;news]==true;]`
 // })
