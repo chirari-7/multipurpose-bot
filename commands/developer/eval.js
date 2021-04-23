afe38a220232316d36632449dd36106218849f61
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-04-23 11:53:25 +0800

    added `eval.js`

---
+++ commands/developer/eval.js
@@ -0,0 +1,11 @@
+module.exports = ({
+    name: "eval",
+    aliases: ['e'],
+    description: "Evaluate a code/function",
+    usage: "eval <code/function>",
+    category: "developer",
+    code: `$eval[$message]
+    $onlyIf[$message[1]!=;{description: Add a message!}{color:RANDOM}]
+    $onlyIf[$checkContains[$channelType;text;news]==true;{description: Please do this in a guild!}{color:RANDOM}]
+    $onlyForIDs[$botOwnerID;{description:❌ You are not my Developer}{color:RANDOM}]`
+})
