a7be14bb0fc74707ac13eaba23c9747e6c9c3c9d
bae༉ <76670103+baeriri@users.noreply.github.com>
2021-05-21 13:57:50 +0800

    Update daily.js

--- commands/economy/daily.js
+++ commands/economy/daily.js
@@ -7,6 +7,6 @@ module.exports = ({
 $setGlobalUserVar[money;$sum[$getGlobalUserVar[money];$random[100;5000]]]
 $color[RANDOM]
 $description[You claimed your daily and got $random[100;10000]💴!]
-$cooldown[1d;{description: You can claim your next daily money in **%time%**!}{color:RANDOM}]
+$globalCooldown[1d;{description: You can claim your next daily money in **%time%**!}{color:RANDOM}]
 $onlyIf[$checkContains[$channelType;text;news]==true;]`
 })
